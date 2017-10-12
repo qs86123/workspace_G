74fcbff6fdaaadda9f0d82b77956b162b050d1d2
wtkid <386427665@qq.com>
2017-10-12 15:53:55 +0800

    遗漏

---
+++ testpro/jpa_springdata_new/src/main/java/com/wt/repositories/JoinFormulaCountryRepository.java
@@ -0,0 +1,12 @@
+package com.wt.repositories;
+
+import com.wt.pojo.JoinFormulaCountry;
+
+/**
+ * @Description
+ * @Author: wangtao
+ * @Date:15:11 2017/10/9
+ * @Email:tao8.wang@changhong.com
+ */
+public interface JoinFormulaCountryRepository extends BaseJpaDao<JoinFormulaCountry>{
+}
